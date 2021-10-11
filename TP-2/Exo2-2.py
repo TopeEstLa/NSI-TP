8881b3a98b82bad4841e9e9c2d4121e4ec8ad74a
TopeEstLa <contact.tope95@gmail.com>
2021-10-11 14:10:35 +0200

    TP2 - Exo2

---
+++ TP-2/Exo2-2.py
@@ -0,0 +1,6 @@
+nbr = int(input("Enter un nombre entier"))
+
+if nbr%2 == 0:
+    print("Le nombre est paire")
+else:
+    print("Le nombre n'est pas paire'")
---
+++ TP-2/Exo2.py
@@ -0,0 +1,5 @@
+nbr = int(input("Enter un nombre entier"))
+
+isPaire = nbr%2 == 0
+
+print(isPaire)
